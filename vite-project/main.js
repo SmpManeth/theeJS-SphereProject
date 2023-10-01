5c01a533caf6a43f1299cd668dff9a2cccafa057
Maneth Pathirana <smpmaneth@gmail.com>
2023-10-01 11:35:09 +0530

    init

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import './style.css';
+import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Scene
 const scene = new THREE.Scene();
@@ -7,7 +9,8 @@ const scene = new THREE.Scene();
 //Create our Speheer
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-	color: '#00ff83',
+  color: '#00ff83',
+  roughness:0.5
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -19,7 +22,7 @@ const sizes = {
 };
 
 // Lights
-const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
+const light = new THREE.PointLight(0xffffff, 70, 100, 1.25);
 light.position.set(0, 10, 10);
 scene.add(light);
 
@@ -37,22 +40,55 @@ scene.add(camera);
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
+
+//Controls
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+controls.rotateSpeed = 0.6;
 
 // resize
 window.addEventListener('resize', () => {
 	//update sizes
 	sizes.width = window.innerWidth;
 	sizes.height = window.innerHeight;
-
-  //update camera
-  camera.updateProjectionMatrix()
+	//update camera
+	camera.updateProjectionMatrix();
 	camera.aspect = sizes.width / sizes.height;
-  renderer.setSize(sizes.width, sizes.height);
-  
+	renderer.setSize(sizes.width, sizes.height);
 });
+
 const loop = () => {
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(loop)
-}
-loop()
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(loop);
+};
+loop();
+
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+timeline.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+timeline.fromTo('nav', { y: '-100%' }, { y: '0%' });
+timeline.fromTo('.title', { opacity: 0 }, { opacity: 1 });
+
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+	if (mouseDown) {
+		rgb = [
+			Math.round((e.pageX / sizes.width) * 255),
+			Math.round((e.pageY / sizes.height) * 255), // Change e.pageX to e.pageY
+			150,
+		];
+		let newColor = new THREE.Color(`rgb(${rgb.join(',')})`); // Use backticks for the template literal
+		gsap.to(mesh.material.color, { r: newColor.r, g: newColor.g, b: newColor.b });
+	}
+});
